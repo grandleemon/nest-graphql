7475ed66da255fbfc55d7d34578c4138ce8bd2af
Maxim <zeetreex@gmail.com>
2024-01-29 20:48:39 +0200

    created and tested a mutation

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,6 +1,7 @@
-import { Resolver, Query, Args, ID } from "@nestjs/graphql";
+import { Resolver, Query, Args, ID, Mutation } from "@nestjs/graphql";
 import { Coffee } from "./entities/coffee.entity/coffee.entity";
 import { ParseIntPipe } from "@nestjs/common";
+import { CreateCoffeeInput } from "./dto/create-coffee.input/create-coffee.input";
 
 @Resolver(() => Coffee)
 export class CoffeesResolver {
@@ -11,6 +12,13 @@ export class CoffeesResolver {
 
   @Query(() => Coffee, { name: "coffee", nullable: true })
   async findOne(@Args("id", { type: () => ID }, ParseIntPipe) id: number) {
+    return null;
+  }
+
+  @Mutation(() => Coffee, { name: "createCoffee", nullable: true })
+  async create(
+    @Args("createCoffeeInput") createCoffeeInput: CreateCoffeeInput,
+  ) {
     return null;
   }
 }
