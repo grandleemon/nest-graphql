2d2cccf71edabc2a99aca379a2b495961ee6b45e
Maxim <zeetreex@gmail.com>
2024-01-29 20:58:16 +0200

    refactored to use service

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -2,23 +2,26 @@ import { Resolver, Query, Args, ID, Mutation } from "@nestjs/graphql";
 import { Coffee } from "./entities/coffee.entity/coffee.entity";
 import { ParseIntPipe } from "@nestjs/common";
 import { CreateCoffeeInput } from "./dto/create-coffee.input/create-coffee.input";
+import { CoffeesService } from "./coffees.service";
 
 @Resolver(() => Coffee)
 export class CoffeesResolver {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
   @Query(() => [Coffee], { name: "coffees" })
   async findAll() {
-    return [];
+    return this.coffeeService.findAll();
   }
 
   @Query(() => Coffee, { name: "coffee", nullable: true })
   async findOne(@Args("id", { type: () => ID }, ParseIntPipe) id: number) {
-    return null;
+    return this.coffeeService.findOne(id);
   }
 
   @Mutation(() => Coffee, { name: "createCoffee", nullable: true })
   async create(
     @Args("createCoffeeInput") createCoffeeInput: CreateCoffeeInput,
   ) {
-    return null;
+    return this.coffeeService.create(createCoffeeInput);
   }
 }
