a02b60bbab01fd69168c29bd4951c65024311dff
Maxim <zeetreex@gmail.com>
2024-01-29 20:16:16 +0200

    passing arguments in a query

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,10 +1,16 @@
-import { Resolver, Query } from "@nestjs/graphql";
+import { Resolver, Query, Args, ID } from "@nestjs/graphql";
 import { Coffee } from "./entities/coffee.entity/coffee.entity";
+import { ParseIntPipe } from "@nestjs/common";
 
 @Resolver(() => Coffee)
 export class CoffeesResolver {
   @Query(() => [Coffee], { name: "coffees" })
   async findAll() {
     return [];
+  }
+
+  @Query(() => Coffee, { name: "coffee", nullable: true })
+  async findOne(@Args("id", { type: () => ID }, ParseIntPipe) id: number) {
+    return null;
   }
 }
